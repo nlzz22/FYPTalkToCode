8c7ecffdd42c11148a94e1ebb680552b2f5830a8
nlzz22 <nlzz_22@hotmail.com>
2017-09-25 16:02:58 +0800

    Added Structural Command Parser python file to pass structured command to code using Martin's program
    Currently passes hello world program by default for testing purposes

---
+++ StructuralCommandParser.py
@@ -0,0 +1,54 @@
+import os.path,subprocess
+from subprocess import STDOUT,PIPE
+
+def main():
+    defaultProgramHeader = "#c_program SampleProgram\n"
+    defaultProgramHeader += "#include #access stdio h #access_end;;"
+
+    # Get Hello World Program for testing
+    defaultProgramHeader += get_sample_hello_world_program_in_structured_command()
+
+    print "Compiling Java Program from Martin\n"
+    compile_java('TalkToCode\\talk-to-code\\src\\ast\\*.java')
+    print "Finished compiling. Running the Java Program... \n"
+    output = execute_java('TalkToCode\\talk-to-code\\src', 'ast/ASTParser', defaultProgramHeader)
+    print "------------------------------------------------------\n"
+
+    # Print the output code
+    print output
+
+
+def compile_java(java_file):
+    subprocess.check_call(['javac', java_file])
+
+def execute_java(java_dir, java_file, stdin):
+    current_dir = os.getcwd()
+
+    # switch directory
+    os.chdir(java_dir)
+
+    # run the java program
+    cmd = ['java ', java_file]
+    proc = subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    
+    # pass input to java program
+    stdout,stderr = proc.communicate(stdin)
+
+    # switch back to original directory
+    os.chdir(current_dir)
+
+    return stdout
+
+def get_sample_hello_world_program_in_structured_command():
+    program_code = "#function_declare main int \n"
+    program_code += "#function_start \n"
+    program_code += "#function printf(#parameter #value \"Hello World!\\n\");; \n"
+    program_code += "return #value 0;; \n"
+    program_code += "#function_end;; \n"
+    program_code += "#program_end\n"
+
+    return program_code
+
+
+# Run the main function
+main()
