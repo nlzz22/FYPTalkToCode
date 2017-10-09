d531cb3530c57df20c354bf25022987e3d3c5072
nlzz22 <nlzz_22@hotmail.com>
2017-10-09 17:40:51 +0800

    Modified main speech to code program to allow user to accept or reject processed text line by line

--- SpeechToCode.py
+++ SpeechToCode.py
@@ -1,48 +1,98 @@
 import os
 import speechrecogniser as SpeechReader
 from WordCorrector import WordCorrector
-from WordParser import WordParser
 from NewWordParser import WordParser as newWordParser
 import StructuralCommandParser as scParser
 
+def get_parsable_portion(original_text, unparsed_text):
+    try:
+        index = original_text.index(unparsed_text)
+
+        actual_inverted_index = len(unparsed_text)
+        parsable_portion = original_text[:len(original_text) - actual_inverted_index]
+
+        return parsable_portion
+    except ValueError: # not found
+        return None
 
 def main():
     to_continue_reading = True
+    struct_cmd_builder = []
     sentences = []
+    unparsed_text = ""
+    #prev_unparsed_text = ""
+    wordParser = newWordParser()
     
-    # Read from audio and process to words
     while to_continue_reading:
+        # Speech to text
         read_words = SpeechReader.get_voice_input()
         if (read_words is None):
             print "Invalid input when reading from audio"
-            return
+            continue
 
-        print "Audio deciphered : " + read_words + "\n"
-        sentences.append(read_words)
+        # text to processed_text
+        wordCorrector = WordCorrector(read_words)
+        corrected = wordCorrector.run_correct_words_multiple("")
 
-        input_continue = raw_input("Continue recording ? (Type 'Y' to continue)\n")
-        if input_continue.lower() != "y":
+        # processed_text to structured_command
+        text_to_parse = ""
+        
+        if unparsed_text is not None and unparsed_text != "":
+            text_to_parse = str(unparsed_text) + " " + str(corrected)
+        else:
+            text_to_parse = str(corrected)
+
+        structured_command = wordParser.parse(str(text_to_parse))
+        current_unparsed_text = wordParser.retrieve_additional_unparsed()
+        
+        parsed_text = get_parsable_portion(text_to_parse, current_unparsed_text) 
+
+        print "=========================================="
+        print "Text already entered: "
+        print "\n".join(sentences)
+        print "=========================================="
+        print "*** Current Context *** "
+        print "Text that can be parsed    : " + parsed_text
+        print "Text that cannot be parsed : " + current_unparsed_text
+        print "=========================================="
+        print "Audio read : " + read_words
+        print "Corrected text : " + corrected
+        print "\n"
+        
+
+        input_continue = raw_input("\nType 'y' to accept and continue, 'n' to reject and continue, 'd' to accept and stop, " + \
+                                   "'t' to reject and stop. \n")
+        if input_continue.lower() == "y":
+            # Accept and continue
+            to_continue_reading = True
+            struct_cmd_builder.append(structured_command)
+            sentences.append(parsed_text)
+            unparsed_text = current_unparsed_text
+        elif input_continue.lower() == "n":
+            # Reject and continue
+            to_continue_reading = True
+        elif input_continue.lower() == "d":
+            # Accept and stop
             to_continue_reading = False
-
-    # join all words together to a complete sentence
-    words = " ".join(sentences)
-    print words
-
-    # Correct words
-    wordCorrector = WordCorrector(words)
-    corrected = wordCorrector.run_correct_words_multiple("")
- 
-    # Convert words to structured command
-    #wordParser = WordParser(corrected)
-    #structured_command = wordParser.map_word_to_structured_command()
-    wordParser = newWordParser()
-    structured_command = wordParser.parse(str(corrected))
+            struct_cmd_builder.append(structured_command)
+            sentences.append(parsed_text)
+            unparsed_text = current_unparsed_text
+        elif input_continue.lower() == "t":
+            # Reject and stop
+            to_continue_reading = False
+        else:
+            to_continue_reading = False # else reject and stop
+            
+    # join all structured_command together to await parsing to code
+    final_struct_command = " ".join(struct_cmd_builder)
+    print "Final structured command : " + final_struct_command
 
     # Convert structured command to code
-    code = scParser.parse_structural_command_to_code(structured_command)
+    code = scParser.parse_structural_command_to_code(final_struct_command)
 
     # Output final code
     print code
     
 # Run the main function
 main()
+
