d21bdc69a1d0c465c2aeea959a0544ede5ef9a0d
nlzz22 <nlzz_22@hotmail.com>
2017-10-24 23:44:04 +0800

    Added python class for reading text files

---
+++ TextFileReader.py
@@ -0,0 +1,22 @@
+class TextFileReader:
+    def __init__(self, filename):
+        self.filename = filename
+        self.lines = self.read_all_lines()
+        self.current_index = 0
+        self.length = len(self.lines)
+
+    def read_all_lines(self):
+        with open(self.filename, "r") as readfile:
+            return readfile.readlines()
+
+    def get_length(self):
+        return self.length
+
+    def read_line(self):
+        if self.current_index < self.get_length():
+            line_to_read = self.lines[self.current_index]
+            self.current_index += 1
+
+            return line_to_read
+        else:
+            return ""
