12af3a51198b9d1f344863fa2a696cb72e04d86a
nlzz22 <nlzz_22@hotmail.com>
2017-10-25 02:15:14 +0800

    Added a temporary script to retrieve test cases from word parser and generate test case for sc parser

---
+++ WordToSCParserGenerator.py
@@ -0,0 +1,17 @@
+with open("NewWordParser.py", "r") as readfile:
+    get_next = False
+    data = readfile.readlines()
+    for line in data:
+        line = line.strip()
+        if get_next:
+            print line
+        elif "struct = " in line and "result_struct" not in line \
+           and "return_struct" not in line:
+            print line
+        else:
+            continue
+        if line[-1:] == "\\":
+            get_next = True
+        else:
+            get_next = False
+            print "print parse_structural_command_to_code(struct)"
