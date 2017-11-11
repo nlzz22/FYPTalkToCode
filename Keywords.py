05bdf4ffa90031e661f32f2d57b838eba84512df
nlzz22 <nlzz_22@hotmail.com>
2017-11-12 02:41:20 +0800

    Updated keywords to be single words

--- Keywords.py
+++ Keywords.py
@@ -1,15 +1,10 @@
 class Keywords:
     def __init__(self):
-        list_keywords = ["equal", "end equal", "array index", "begin if", "greater than", "greater than equal"]
-        list_keywords += ["less than", "less than equal", "not equal", "then", "else", "end if", "for", "loop"]
-        list_keywords += ["condition", "begin", "plus", "minus", "declare", "integer", "float", "double", "long"]
-        list_keywords += ["end for", "times", "divide", "modulo", "end declare", "array", "with", "size", "return"]
-        list_keywords += ["void", "create function", "return type", "parameter", "end function", "while", "end while"]
-        list_keywords += ["call function", "type", "string", "end string", "character"]
+        list_keywords = ["array", "begin", "call", "character", "condition", "create", "declare", "divide"]
+        list_keywords += ["double", "else", "end", "equal", "float", "for", "function", "greater", "if", "index", "integer"]
+        list_keywords += ["less", "long", "loop", "minus", "modulo", "not", "parameter", "plus", "return", "size", "string"]
+        list_keywords += ["than", "then", "times", "type", "void", "while", "with"]
         self.keywords = list_keywords
     
     def get_keywords(self):
         return self.keywords
-
-
-
