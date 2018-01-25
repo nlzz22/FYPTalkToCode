3674b2befc4a2dd532363ae7b02e8aeeed899cd0
nlzz22 <nlzz_22@hotmail.com>
2018-01-25 19:14:56 +0800

    cleanup.py now cleans up memory intensive process as well

--- cleanup.py
+++ cleanup.py
@@ -4,7 +4,7 @@ PROCNAME = "pythonw.exe"
 
 for proc in psutil.process_iter():
     if proc.name() == PROCNAME:
-        if proc.cpu_percent(interval=0.1) > 20:
+        if proc.cpu_percent(interval=0.1) > 20 or proc.memory_percent() > 0.90:
             proc.terminate()
             print ("Terminated the following process:")
             print(proc)
