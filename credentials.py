9045b4e971d71fb886a35c43cd3bbf787798851c
nlzz22 <nlzz_22@hotmail.com>
2018-02-21 18:47:48 +0800

    Removed unused bing functions in credentials.py

--- credentials.py
+++ credentials.py
@@ -5,12 +5,7 @@ import json
 
 class APICredentials:
     GOOGLE_CREDENTIAL_FILE = "SpeechRecognition-8e50e15b5266.json"
-    BING_FILE = "Bing.txt"
 
     def get_google_json_file(self):
         with open(self.GOOGLE_CREDENTIAL_FILE) as file:
             return json.dumps(json.load(file))
-
-    def get_bing_key(self):
-        with open(self.BING_FILE) as file:
-            return file.readline()
