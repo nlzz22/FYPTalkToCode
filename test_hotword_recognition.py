54855ced3f167ff0a38c81ae0c762c9f1efb3e93
nlzz22 <nlzz_22@hotmail.com>
2018-03-01 18:10:04 +0800

    added a python file to test hotword recognition
    Note: this file is not used in the main program, merely for testing purposes only

---
+++ test_hotword_recognition.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+r.energy_threshold = 1000
+
+while True:
+    with sr.Microphone() as source:
+        #print("Say something!")
+        try:
+            audio = r.listen(source,timeout=0.1,phrase_time_limit =3)
+        except:
+            continue
+
+    # recognize speech using Sphinx
+    keyword_entries = [["start recording", 1e-3], ["ah", 1e-49], ["start rack cording", 1e-4], ["stuck recording", 1e-4] ,\
+                       ["stop right", 1e-5]]
+
+    try:
+        a =  r.recognize_sphinx(audio, keyword_entries=keyword_entries)
+        print a 
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
